c1743ee1af8eb24be1424437108cc643f1b45574
GunHo <noh3310@gmail.com>
2022-03-12 21:04:24 +0900

    오픈채팅방 문제 해결

---
+++ Algorithm_Swift/프로그래머스/오픈채팅방.swift
@@ -0,0 +1,37 @@
+//
+//  오픈채팅방.swift
+//  Algorithm_Swift
+//
+//  Created by 노건호 on 2022/03/12.
+//
+
+import Foundation
+
+func solution(_ record: [String]) -> [String] {
+    var result: [String] = []
+    var dic: [String: String] = [:]
+    
+    record.forEach {
+        let text = $0.split(separator: " ").map{ String($0) }
+        // Enter이랑, Change할 때 닉네임 변경하기
+        if text[0] == "Enter" || text[0] == "Change" {
+            dic.updateValue(text[2], forKey: text[1])
+        }
+    }
+    
+    record.forEach {
+        let text = $0.split(separator: " ").map{ String($0) }
+        
+        if text[0] == "Enter" {
+            result.append("\(dic[text[1]]!)님이 들어왔습니다.")
+        } else if text[0] == "Leave" {
+            result.append("\(dic[text[1]]!)님이 나갔습니다.")
+        }
+    }
+    
+    return result
+}
+
+let result = solution(["Enter uid1234 Muzi", "Enter uid4567 Prodo","Leave uid1234","Enter uid1234 Prodo","Change uid4567 Ryan"])
+
+print(result)
