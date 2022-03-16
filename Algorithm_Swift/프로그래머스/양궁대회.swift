5f56a75024f35528535526480794b738472793d0
GunHo <noh3310@gmail.com>
2022-03-16 16:43:57 +0900

    양궁대회

--- Algorithm_Swift/프로그래머스/양궁대회.swift
+++ Algorithm_Swift/프로그래머스/양궁대회.swift
@@ -7,13 +7,13 @@
 
 import Foundation
 
-func checkScore(_ array: [Int]) -> [Int] {
+private func checkScore(_ array: [Int]) -> [Int] {
     var apeachScore = 0
     var myScore = 0
     for i in array.indices {
         // 어피치가 점수가 같거나 큰 경우
         // 둘다 0점이아닌 경우
-        if apeach[i] != 0 && array[i] != 0 {
+        if apeach[i] != 0 || array[i] != 0 {
             if apeach[i] >= array[i] {   // 어피치가 같거나 큰 경우
                 apeachScore += 10 - i
             } else if apeach[i] < array[i] {    // 내가 점수가 큰 경우
@@ -25,29 +25,28 @@ func checkScore(_ array: [Int]) -> [Int] {
     return [apeachScore, myScore]
 }
 
-func dfs(_ position: Int, _ array: [Int], _ n: Int) {
-    // 0점 이하는 쏠 수 없다.
-    if position > 10 {
-        return
-    }
+private func dfs(_ position: Int, _ array: [Int], _ n: Int) {
     
     // 화살을 전부다 쐈을 때 이겼는지 점수계산
     if n == 0 {
-        print(array)
         let score = checkScore(array)
-        if (score[1] - score[0] >= difference || difference == -1) && score[1] > score[0] {
-            if resultArray == [-1] {
+        // (점수가 최고 점수차랑 같거나 큰 경우 || 처음 들어온 경우) && 라이언이 점수가 더 높을 때
+        if ((score[1] - score[0] >= difference) || (difference == -1)) && score[1] > score[0] {
+            // 처음 점수를 추가하거나, 점수차이가 지금 가지고있는 점수차보다 큰 경우
+            if resultArray == [-1] || score[1] - score[0] > difference {
                 resultArray = array
                 difference = score[1] - score[0]
-            } else {
+            } else {    // 점수가 같은 경우
                 var check = false
-                // 점수가 낮은 화살을 누가 더 많이 쐈는지 검사
-                for i in stride(from: 10, to: -1, by: -1) {
-                    if resultArray[i] < array[i] {
-                        check = true
-                        break
+                    // 점수가 낮은 화살을 누가 더 많이 쐈는지 검사
+                    for i in stride(from: 10, to: -1, by: -1) {
+                        if resultArray[i] < array[i] {
+                            check = true
+                            break
+                        } else if resultArray[i] > array[i] {
+                            break
+                        }
                     }
-                }
 
                 if check {
                     resultArray = array
@@ -58,10 +57,15 @@ func dfs(_ position: Int, _ array: [Int], _ n: Int) {
         return
     }
     
+    // index가 벗어났을 때 남은 화살 개수가 있다면 return
+    if position > 10 && n > 0 {
+        return
+    }
+    
     // 다음 과녁으로 넘어가는 것
     for i in 0...n {
         // 쏠 화살 개수가 남아있다면 && 어피치보다 1점 많을떄까지만 화살 쏨
-        if n - i >= 0/* && apeach[position] + 1 >= i*/ {
+        if n - i >= 0 {
             var temp = array
             temp[position] = i
             dfs(position + 1, temp, n - i)
@@ -95,7 +99,7 @@ func solution(_ n:Int, _ info:[Int]) -> [Int] {
 
 let n = 10
 let info = [0,0,0,0,0,0,0,0,3,4,3]
-// [1,1,1,1,1,1,1,1,0,0,2]
+//// [1,1,1,1,1,1,1,1,0,0,2]
 
 let result = solution(n, info)
 
